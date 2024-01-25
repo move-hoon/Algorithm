e120657fde5b83034a19d9cd384fef3e129945e2
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-01-25 23:51:08 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
