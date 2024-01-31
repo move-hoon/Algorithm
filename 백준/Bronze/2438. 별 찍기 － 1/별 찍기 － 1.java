02963ea53ef23bf0f1a93340591648ba10c32935
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-01-31 23:59:11 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 300 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int count = kb.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            for (int j = count; j > count - i - 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
