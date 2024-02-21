fefec695199544f63fe48be538eff1abcc278523
DongHoon Lee <125895298+hoonyworld@users.noreply.github.com>
2024-02-21 23:47:35 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+        int c = kb.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c))%c);
+    }
+}
